49ad5b022d7266223c74a744396e3fd75d067f25
EdgeSlayer1997 <EdgeSlayer97@gmail.com>
2024-12-03 15:29:13 +0600

    Corrección de rutas

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE_NAME = "mi-portafolio-cache-v21";
-const urlsToCache = ["index.html", "styles.css", "script.js", "manifest.json"];
+const CACHE_NAME = "mi-portafolio-cache-v22";
+const urlsToCache = ["index.html", "styles.css", "script.js", "manifest.json", './icons/icon-192x192.png',
+  './icons/icon-512x512.png',];
 
 self.addEventListener("install", (event) => {
     event.waitUntil(
